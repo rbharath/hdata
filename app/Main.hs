b9c614e777fe6423304485c91539d4043f2217d5
Bharath Ramsundar <rbharath@stanford.edu>
2017-05-06 15:23:35 -0700

    Some basic experiments

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,20 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = someFunc
+import Numeric.Datasets (getDataset)
+import Numeric.Datasets.Iris (iris, irisClass)
+import Numeric.Datasets.Abalone (abalone)
+import Numeric.LinearAlgebra
+
+main = do
+
+  -- The Iris data set is embedded
+  print (length iris)
+  print (head iris)
+  let irisClasses = map irisClass iris
+  print irisClasses
+
+  -- The Abalone dataset is fetched
+  abas <- getDataset abalone
+  print (length abas)
+  print (head abas)
